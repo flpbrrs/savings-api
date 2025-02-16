53ff39fc78cabcebf23eb3dc38a6fa8082251489
Felipe Barros <felipejonathan.dev@gmail.com>
2025-02-16 19:44:49 -0300

    feat: criação do controller de login

---
+++ src/controllers/auth/loginController.ts
@@ -0,0 +1,25 @@
+import { Express } from "express"
+import Login from "../../core/auth/services/login";
+
+export class loginController {
+    constructor(
+        private readonly app: Express,
+        private readonly casoDeUso: Login,
+        ...middlewares: any[]
+    ) {
+        app.post('/login', async (request, response) => {
+            try {
+                const { email, senha } = request.body
+                const token = await casoDeUso.execute(email, senha)
+
+                response.status(200).json({
+                    data: { token }
+                })
+            } catch (e: any) {
+                response.status(401).json({
+                    erros: e.message
+                })
+            }
+        })
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import app from "./adapters/api/config";
 import BcryptDataEncrypter from "./adapters/auth/BcryptDataEncrypter";
+import JWTTokenGenerator from "./adapters/auth/JWTTokenGenerator";
 import PrismaUserRepository from "./adapters/db/PrismaUserRepository";
+import { loginController } from "./controllers/auth/loginController";
 import { registrarUsuarioController } from "./controllers/auth/registrarUsuárioController";
+import Login from "./core/auth/services/login";
 import RegistrarUsuario from "./core/auth/services/registrarUsuario";
 
 (async () => {
     const userRepository = new PrismaUserRepository()
     const dataEncrypter = new BcryptDataEncrypter()
+    const jwtProvider = new JWTTokenGenerator()
 
     new registrarUsuarioController(app, new RegistrarUsuario(
         userRepository,
         dataEncrypter
+    ))
+    new loginController(app, new Login(
+        userRepository,
+        dataEncrypter,
+        jwtProvider
     ))
 })()
