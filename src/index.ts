728d56022870cb4dceab03ac4fa3fab7f63b42e1
Felipe Barros <felipejonathan.dev@gmail.com>
2025-02-16 14:44:02 -0300

    feat: disponibilizando endpoint de criação de usuários

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Server is runnig')
+import app from "./adapters/api/config";
+import BcryptDataEncrypter from "./adapters/auth/BcryptDataEncrypter";
+import PrismaUserRepository from "./adapters/db/PrismaUserRepository";
+import { registrarUsuarioController } from "./controllers/auth/registrarUsuárioController";
+import RegistrarUsuario from "./core/auth/services/registrarUsuario";
+
+(async () => {
+    const userRepository = new PrismaUserRepository()
+    const dataEncrypter = new BcryptDataEncrypter()
+
+    new registrarUsuarioController(app, new RegistrarUsuario(
+        userRepository,
+        dataEncrypter
+    ))
+})()
