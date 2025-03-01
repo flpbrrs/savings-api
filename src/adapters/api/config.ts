5225ecf28c4fb1a7037d46d4951c5a1bdeadeb15
Felipe Barros <felipejonathan.dev@gmail.com>
2025-03-01 07:39:30 -0300

    build: adicionando dependência do cors e adicionando configuração inicial

--- src/adapters/api/config.ts
+++ src/adapters/api/config.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 
 const PORT = process.env.API_PORT ?? 5000
 
@@ -6,6 +7,10 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200
+}))
 
 app.get('/ping', (_, res) => {
     res.send('pong')
